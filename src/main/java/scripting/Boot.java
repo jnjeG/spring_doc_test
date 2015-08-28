23bdb3cf9474dc8aa6c1a7170cc4d9b15661f821
jneG <jnje123ysdy>
2015-08-28 13:52:19 +0800

    Example: Hello World, BeanPostProcessor-style

---
+++ src/main/java/scripting/Boot.java
@@ -0,0 +1,14 @@
+package scripting;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public final class Boot {
+
+    public static void main(final String[] args) throws Exception {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("scripting/beans.xml");
+        Messenger messenger = (Messenger) ctx.getBean("messenger");
+        System.out.println(messenger);
+    }
+
+}
